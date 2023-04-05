0cd4df77001d24e6bb7370a0d06d8b9c6ee4b278
Jason Ku <jasonku@LynndeMacBook-Air.local>
2023-04-05 15:50:04 +0800

    feat: Implement Slider (Support both touch/mouse event)

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,32 +1,64 @@
 import React, { useState } from "react";
-import { nanoid } from "nanoid";
 import styles from "./Slider.module.scss";
 
-// TODO: Implement mobile friendly slider
 export default function Slider({ data }) {
-  const totalPage = 4;
+  const sliderLen = data.length;
   const [pageIdx, setPageIdx] = useState(0);
+  const [touchX, setTouchX] = useState(null);
 
-  const handleDrag = (e) => {
-    e.preventDefault();
+  const handleChangeSlideStart = (e) => {
+    let touchDown;
+    if (e.type === "mousedown") {
+      touchDown = e.clientX;
+    } else if (e.type === "touchstart") {
+      touchDown = e.touches[0].clientX;
+    }
+    setTouchX(touchDown);
+  };
+
+  const handleChangeSlideEnd = (e) => {
+    const touchDown = touchX;
+    if (touchDown === null) {
+      return;
+    }
+
+    let currentTouch;
+    if (e.type === "mouseup") {
+      currentTouch = e.clientX;
+    } else if (e.type === "touchmove") {
+      currentTouch = e.touches[0].clientX;
+    }
+    const diff = touchDown - currentTouch;
+    if (diff > 10) {
+      setPageIdx((prev) => (prev === sliderLen - 1 ? prev : ++prev));
+    }
+    if (diff < -10) {
+      setPageIdx((prev) => (prev === 0 ? prev : --prev));
+    }
+    setTouchX(null);
   };
 
   return (
     <div className={styles.slider}>
-      <div className={styles.frame}>
+      <div
+        className={`${styles.container}`}
+        style={{ transform: `translateX(${"-" + pageIdx * 100 + "%"})` }}
+        onTouchStart={handleChangeSlideStart}
+        onTouchMove={handleChangeSlideEnd}
+        onMouseDown={handleChangeSlideStart}
+        onMouseUp={handleChangeSlideEnd}
+      >
         {data.map((p, i) => (
           <img
-            key={nanoid()}
+            key={i}
             src={p}
-            alt={`slider ${i}`}
+            alt={`slide ${i + 1}`}
             className={styles.slide}
-            onDrag={handleDrag}
-            onTouchMove={handleDrag}
-            draggable
+            draggable={false}
           />
         ))}
-        <div className={styles.pageNum}>{`${pageIdx + 1}/${totalPage}`}</div>
       </div>
+      <div className={styles.pageNum}>{`${pageIdx + 1}/${sliderLen}`}</div>
     </div>
   );
 }
